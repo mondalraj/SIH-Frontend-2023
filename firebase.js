74620b729b73497d8d4791b6b07bf96c78e02c35
Pranshu321 <pranshujain0111@gmail.com>
2023-09-10 13:07:22 +0530

    Added Firebase Config

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5YwR103nvITKPlOdMzJkWZfBMyu4ncoQ",
+  authDomain: "legi-companion.firebaseapp.com",
+  projectId: "legi-companion",
+  storageBucket: "legi-companion.appspot.com",
+  messagingSenderId: "873431152146",
+  appId: "1:873431152146:web:4f8b5040294b58d9534acb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+export default app;
+export { auth, db };
